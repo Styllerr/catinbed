b51d7dd42be59bd8729e188e3256b74c1e4c5188
Evgeniy Chukavov <cork@ukr.net>
2021-05-18 16:10:28 +0300

    add event

--- script/create-card.js
+++ script/create-card.js
@@ -26,17 +26,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const createCards = (product) => {
         const carouselName = document.getElementById(product);
         const list = carouselName.querySelector('.ant-carousel-list');
-        list.addEventListener('click', (evnt) => {
-            console.log(evnt.target)
+        list.addEventListener('click', (event) => {
+            console.log(event.target.id)
         })
         const fragment = document.createDocumentFragment();
         if(product === "mattress") {
             mattressList.forEach(item => {
                 let tempNode = document.createElement('li');
                 tempNode.className = 'ant-carousel-element';
-                tempNode.innerHTML = `<div id=${item.id} class="card__wrapper">
-                                    <h2 class="card__title">${item.name}</h2><img class="card__img"
-                                    src="${item.imgPath}">
+                tempNode.innerHTML = `<div class="card__wrapper">
+                                    <h2 class="card__title">${item.name}</h2>
+                                    <img id=${item.id} class="card__img" src="${item.imgPath}">
                                     <p class="price__wrapper">Цена: <span class="price">${item.price}</span></p>
                                     </div>`;
                 fragment.appendChild(tempNode);
@@ -46,10 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
             pillowList.forEach(item => {
                 let tempNode = document.createElement('li');
                 tempNode.className = 'ant-carousel-element';
-                tempNode.innerHTML = `<div id=${item.id} class="card__wrapper pillow-card">
+                tempNode.innerHTML = `<div class="card__wrapper pillow-card">
                 <h2 class="card__title">${item.tradeMark}</h2>
                 <p class="card__size">${item.size[0]} <span>x</span> ${item.size[1]} <span>x</span> ${item.size[2]}</p>
-                <img class="card__img" src="${item.imgPath}" alt="Pillow image">
+                <img id=${item.id} class="card__img" src="${item.imgPath}" alt="Pillow image">
                 <h2 class="card__name">${item.name}</h2>
                 <p class="price__wrapper">Цена: <span class="price">${item.price}</span></p>
             </div>`;
