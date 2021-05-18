5dd03f7cbdf22e70c4e0d6afd8b04de25d685865
Evgeniy Chukavov <cork@ukr.net>
2021-05-18 14:43:52 +0300

    add schema carousel

--- script/create-card.js
+++ script/create-card.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let mattressList = [];
     let pillowList = [];
     let commentList = [];
+
+    
     fetch('./data/mattress.json')
         .then((response) => {
             return response.json();
@@ -23,7 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const createCards = (product) => {
         const carouselName = document.getElementById(product);
-        const list = carouselName.querySelector('.ant-carousel-list')
+        const list = carouselName.querySelector('.ant-carousel-list');
+        list.addEventListener('click', (evnt) => {
+            console.log(evnt.target)
+        })
         const fragment = document.createDocumentFragment();
         if(product === "mattress") {
             mattressList.forEach(item => {
@@ -41,11 +46,13 @@ document.addEventListener('DOMContentLoaded', () => {
             pillowList.forEach(item => {
                 let tempNode = document.createElement('li');
                 tempNode.className = 'ant-carousel-element';
-                tempNode.innerHTML = `<div id=${item.id} class="card__wrapper">
-                                    <h2 class="card__title">${item.name}</h2><img class="card__img"
-                                    src="${item.imgPath}">
-                                    <p class="price__wrapper">Цена: <span class="price">${item.price}</span></p>
-                                    </div>`;
+                tempNode.innerHTML = `<div id=${item.id} class="card__wrapper pillow-card">
+                <h2 class="card__title">${item.tradeMark}</h2>
+                <p class="card__size">${item.size[0]} <span>x</span> ${item.size[1]} <span>x</span> ${item.size[2]}</p>
+                <img class="card__img" src="${item.imgPath}" alt="Pillow image">
+                <h2 class="card__name">${item.name}</h2>
+                <p class="price__wrapper">Цена: <span class="price">${item.price}</span></p>
+            </div>`;
                 fragment.appendChild(tempNode);
             })
             list.appendChild(fragment);
